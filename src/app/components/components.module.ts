d44ed3fcf4af2a1e36dffa925c2671102503dc8f
minhlienb <minhlien2077@gmail.com>
2024-10-09 00:27:04 +0700

    add components module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { ProductCardComponent } from "./product-card/product-card.component";
+
+@NgModule({
+    declarations: [ProductCardComponent],
+    exports: [ProductCardComponent]
+})
+export class ComponentsModule {}   
