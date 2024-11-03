82fd35a5dd0de0904be3f33846225aac5cc9c4b0
minhlienb <minhlien2077@gmail.com>
2024-11-03 22:22:00 +0700

    thêm trang chi tiết hóa đơn

--- src/app/invoice-detail/invoice-detail.page.ts
+++ src/app/invoice-detail/invoice-detail.page.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './invoice-detail.page.html',
   styleUrls: ['./invoice-detail.page.scss'],
 })
-export class InvoiceDetailPage implements OnInit {
 
-  constructor() { }
+export class InvoiceDetailPage {
+  order = {
+    danhGia: 0,
+    hoaDon: "9da2bf7c-979f-484b-a2a6-bba8eee953d3",
+    ngayMua: "3/11/2024",
+    products: [
+      {
+        category: "Drug",
+        name: "Viên uống Pikolin Ocavill hỗ trợ tăng tuần hoàn máu não",
+        price: 100000,
+        product: "productId_01",
+        quantity: 1,
+      },
+      {
+        category: "Drug",
+        name: "Viên uống Omexxel Ginkgo 120 Premium Omexxel hỗ trợ trí nhớ",
+        price: 120000,
+        product: "productId_02",
+        quantity: 1,
+      }
+    ],
+    thanhTien: 220000,
+  };
 
-  ngOnInit() {
+
+  reoder(/** params*/) {
+    console.log('Mua lại hóa đơn');
   }
-
 }
+
