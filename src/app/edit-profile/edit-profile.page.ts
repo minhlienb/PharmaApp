74119f4fca19e31be615b7a07e5a42060f1f3266
minhlienb <minhlien2077@gmail.com>
2024-11-03 15:39:42 +0700

    sửa giao UI, thêm một số logic thô

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController,
+  ) { }
 
   ngOnInit() {
   }
+  
+  UpdateProfile() {
+    let error = 1;
+    if (error) {
+      this.presentAlerts("Thông báo thành công/Lỗi sai thông tin, sai mật khẩu, mật khẩu không khớp, mật khẩu quá ngắn, ...");
+    }
+  }
 
+  async presentAlerts(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Thông báo',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
