d8324acc58f20d16a437743e7126bf2f28eecb7c
Le Tuan Nghia <ltn26052003@gmail.com>
2024-11-05 03:08:38 +0700

    Feat: Update, Read info & add new address

--- src/app/Model/reviews/reviews.model.ts
+++
@@ -1,9 +0,0 @@
-export class ReviewsModel {
-    constructor(
-        public productId: string,
-        public userId: string,
-        public rating: number,
-        public comment: string,
-        public reviewDate: Date
-    ) { }
-}
